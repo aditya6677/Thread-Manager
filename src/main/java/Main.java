4fde100d98d8872ee01e18a73ffc61e361b71d46
aditya6677 <6677aditya@gmail.com>
2024-06-17 19:29:55 +0530

    Implementing Thread, Context Switching Occurs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    public static void main(String[] args) {
+        ThreadTask threadTask = new ThreadTask();
+        System.out.println(Thread.currentThread().getName());
+        threadTask.start();
+        //Context Switching may occur
+        RunnableThread runnableThread = new RunnableThread();
+        Thread thread = new Thread(runnableThread);
+        thread.start();
+    }
 }
--- src/main/java/ThreadTask.java
+++ src/main/java/ThreadTask.java
@@ -1,2 +1,8 @@
-public class ThreadTask {
+public class ThreadTask extends Thread{
+
+    public void run(){
+        for(int i=0; i<10; i++){
+            System.out.println("Running by " + Thread.currentThread().getName());
+        }
+    }
 }
